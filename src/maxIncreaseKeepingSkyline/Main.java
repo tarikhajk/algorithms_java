29db8cbd265c6d6a69cfdf012a03ffdaee433b04
Tarik <tarikh@uw.edu>
2018-11-11 16:17:33 -0800

    Adding a few comments to Main.java

--- src/maxIncreaseKeepingSkyline/Main.java
+++ src/maxIncreaseKeepingSkyline/Main.java
@@ -8,5 +8,6 @@ public class Main {
 		int[][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
 		int result = new Solution().maxIncreaseKeepingSkyline(grid);
 		System.out.println(result);
+		//excpeted is 35
 	}
 }
